45eca0e791415e145974b0b289c9a3fb89ae177b
Erik <ogaroerick1998@gmail.com>
2019-12-26 19:25:02 +0300

    CLI Arguments - argv

---
+++ ex13.py
@@ -0,0 +1,10 @@
+#  arguments, parameters, unpacking and variables
+
+from sys import argv
+# read the WYSS  section for more
+script_name, first_variable, second_variable, third_variable = argv
+
+print("This script is called: ", script_name)
+print("Your first variable is: ", first_variable)
+print("Your second variable is: ", second_variable)
+print("Your third variable is called: ", third_variable)
