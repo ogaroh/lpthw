27090070710f7d4ec975c727a194e0f54625e0c1
Erik <ogaroerick1998@gmail.com>
2019-12-26 19:58:30 +0300

    reading from files

--- ex15.py
+++ ex15.py
@@ -3,12 +3,12 @@ from sys import argv
 # uses argv to get file names --> method 1 
 script_name, filename = argv
 
-# open the file specified
+# creates the file object so that you can read or write to it
 txt = open(filename)
 
 print(f"here is your {filename} file.")
 
-# prints out the contents of the file specified
+# shows the contents of the file specified
 print(txt.read())
 
 # requires user to manually enter the file name --> method 2
