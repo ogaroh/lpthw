fa78b9384e0293525fde9df049d9bdb6cedadb4d
Erik <ogaroerick1998@gmail.com>
2019-12-27 17:51:42 +0300

    made the script shorter by 10+ lines 🤯

--- test_args.py
+++ test_args.py
@@ -5,5 +5,4 @@ script_name, first_name, last_name = argv
 yob = int(input("When were you born? "))
 age = 2020 - yob
 
-print("Your full name is ", first_name, last_name, " and you're approximately ", age, " years old.")
-
+print("Your full name is ", first_name, last_name, " and you're approximately ", int(age), " years old.")
